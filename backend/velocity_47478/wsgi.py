77f996691052d777a08d01652fe6c1dd72124aca
Crowdbotics <team@crowdbotics.com>
2024-04-11 21:26:08 +0000

    Initial commit

---
+++ backend/velocity_47478/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for velocity_47478 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "velocity_47478.settings"
+)
+
+application = get_wsgi_application()
